39976a393402304dbb88c89b195aaefad92b1f16
Karen Khalifah <karen.khalifah@gmail.com>
2019-06-15 22:41:49 -0400

    social and styling. on off switch video button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
-    
+    //preloader
     $('.preloader').fadeOut();
    
+    //nav-btn
+    $('.nav-btn').click(function(){
+        $('.nav').toggleClass('nav-show')
+    });
     
-    
-    
+    // video slider
+    $('.video-btn-container').click(function(){
+        const value = $('.video-switch-btn').hasClass('btn-slide')
+        if(value) {
+            $('.video-switch-btn').removeClass('btn-slide')
+            $('#video').get(0).play()
+            
+        } else {
+            $('.video-switch-btn').addClass('btn-slide')
+            $('#video').get(0).pause()
+        }
+    });
     
     
     
